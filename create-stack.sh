ecb1cec6cda011ad9501bd7a6fd9d8165318c6c6
Lachlan Donald <lachlan@ljd.cc>
2016-01-25 15:25:03 +1100

    Update to docker 1.9, remove provision bucket

--- create-stack.sh
+++ create-stack.sh
@@ -13,20 +13,16 @@ if [[ $# -lt 3 ]] ; then
   exit 1
 fi
 
-# grab the org slug to generate the provision bucket default
-if [[ "$*" =~ BuildkiteOrgSlug=([0-9a-zA-Z_\-]+) ]]; then
-  ORG_SLUG=${BASH_REMATCH[1]}
-elif [[ -z $PROVISION_BUCKET ]]; then
-  echo "Must provide either BuildkiteOrgSlug parameter or set a PROVISION_BUCKET env"
+if [[ ! "$*" =~ BuildkiteOrgSlug=([0-9a-zA-Z_\-]+) ]]; then
+  echo "Must provide a BuildkiteOrgSlug parameter"
   exit 1
 fi
 
-PROVISION_BUCKET=${PROVISION_BUCKET:-${ORG_SLUG}-buildkite}
 STACK_NAME=${STACK_NAME:-buildkite-$(date +%Y-%m-%d-%H-%M)}
 STACK_TEMPLATE="$(dirname $0)/build/cloudformation.json"
 
 cd $(dirname $0)
-make all
+make all > /dev/null
 
 # sometimes cfoo returns success but errors
 if [[ ! -s $STACK_TEMPLATE ]] ; then
@@ -35,13 +31,17 @@ if [[ ! -s $STACK_TEMPLATE ]] ; then
 fi
 
 PARAMS=$(build_parameters "$@")
-PARAMS+=" ParameterKey=ProvisionBucket,ParameterValue=$PROVISION_BUCKET"
+
+echo ">> Creating stack $STACK_NAME from $STACK_TEMPLATE"
 
 # http://docs.aws.amazon.com/cli/latest/reference/cloudformation/create-stack.html
-echo "Creating cfn stack ${STACK_NAME}"
-aws cloudformation create-stack \
+stack_id=$(aws cloudformation create-stack \
+  --output text \
   --stack-name ${STACK_NAME} \
   --disable-rollback \
   --template-body "file://${STACK_TEMPLATE}" \
   --capabilities CAPABILITY_IAM \
-  --parameters $PARAMS
+  --parameters $PARAMS)
+
+echo -e "\033[33;32m$stack_id\033[0m\n"
+
