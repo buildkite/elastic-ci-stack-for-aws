b3d6aebc8e15dcccf6177c03f6b9401beed4ef3a
Lachlan Donald <lachlan@ljd.cc>
2016-02-18 09:58:20 +1100

    Ensure packer step fails when packer does

--- ci/packer.sh
+++ ci/packer.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -eu
 
+set -o pipefail
+
 cd $(dirname $0)/../packer/
 
 packer validate buildkite-ubuntu-15.04.json
-packer build -machine-readable buildkite-ubuntu-15.04.json | tee packer.output
+packer build buildkite-ubuntu-15.04.json | tee packer.output
 buildkite-agent artifact upload packer.output
