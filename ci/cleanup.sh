967194648c1cfa053fc90fa2e95a2b8fd9bc93b0
Lachlan Donald <lachlan@ljd.cc>
2016-02-23 15:12:38 +1100

    Show errors in cleanup

--- ci/cleanup.sh
+++ ci/cleanup.sh
@@ -8,4 +8,4 @@ echo "--- Deleting stack $stack_name"
 aws cloudformation delete-stack --stack-name "$stack_name"
 
 echo "--- Deleting pipeline $bk_pipeline_slug"
-curl --silent -f -X DELETE "https://api.buildkite.com/v2/organizations/$BUILDKITE_AWS_STACK_ORG_SLUG/pipelines/$bk_pipeline_slug"
+curl --show-error --silent -f -X DELETE "https://api.buildkite.com/v2/organizations/$BUILDKITE_AWS_STACK_ORG_SLUG/pipelines/$bk_pipeline_slug"
