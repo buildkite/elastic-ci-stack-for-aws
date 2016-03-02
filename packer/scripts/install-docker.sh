38562d01babbcbda1d51aec64d25d0686f0390ea
Lachlan Donald <lachlan@ljd.cc>
2016-03-02 12:13:49 +1100

    Add back support for DOCKER_HUB_AUTH

--- packer/scripts/install-docker.sh
+++ packer/scripts/install-docker.sh
@@ -12,7 +12,11 @@ sudo docker info
 sudo curl -o /usr/bin/docker-compose -L https://github.com/docker/compose/releases/download/1.5.0/docker-compose-Linux-x86_64
 sudo chmod +x /usr/bin/docker-compose
 
-# # install docker-gc
+# install docker-gc
 curl -L https://raw.githubusercontent.com/spotify/docker-gc/master/docker-gc > docker-gc
 sudo mv docker-gc /etc/cron.hourly/docker-gc
 sudo chmod +x /etc/cron.hourly/docker-gc
+
+# install jq
+sudo curl -o /usr/bin/jq -L https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64
+sudo chmod +x /usr/bin/jq
