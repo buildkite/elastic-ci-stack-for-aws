c1c77acea64d35d60696afa4800574174bc312c6
Lachlan Donald <lachlan@ljd.cc>
2016-05-05 13:20:25 +1000

    Add lifecycled configuration

--- packer/scripts/install-lifecycled.sh
+++ packer/scripts/install-lifecycled.sh
@@ -1,10 +1,17 @@
 #!/bin/bash -eu
 
-touch /etc/lifecycled
-
-curl -Lf -o /usr/bin/lifecycled \
+sudo touch /etc/lifecycled
+sudo curl -Lf -o /usr/bin/lifecycled \
 	https://github.com/lox/lifecycled/releases/download/v1.0.0/lifecycled-linux-x86_64
-chmod +x /usr/bin/lifecycled
+sudo chmod +x /usr/bin/lifecycled
 
-curl -Lf -o /etc/init/lifecycled.conf \
+sudo curl -Lf -o /etc/init/lifecycled.conf \
 	https://raw.githubusercontent.com/lox/lifecycled/v1.0.0/init/upstart/lifecycled.conf
+
+cat << EOF | sudo tee /usr/bin/buildkite-lifecycle-handler
+#!/bin/sh -eu
+stop buildkite-agent
+sleep 25
+EOF
+
+sudo chmod +x /usr/bin/buildkite-lifecycle-handler
