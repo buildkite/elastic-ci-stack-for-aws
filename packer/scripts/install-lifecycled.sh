f1c1937c7889d49d021674cbeb5dfa09ae111211
Lachlan Donald <lachlan@ljd.cc>
2016-05-06 12:10:17 +1000

    Bump lifecycled version

--- packer/scripts/install-lifecycled.sh
+++ packer/scripts/install-lifecycled.sh
@@ -2,16 +2,20 @@
 
 sudo touch /etc/lifecycled
 sudo curl -Lf -o /usr/bin/lifecycled \
-	https://github.com/lox/lifecycled/releases/download/v1.0.0/lifecycled-linux-x86_64
+	https://github.com/lox/lifecycled/releases/download/v1.1.0/lifecycled-linux-x86_64
 sudo chmod +x /usr/bin/lifecycled
 
 sudo curl -Lf -o /etc/init/lifecycled.conf \
-	https://raw.githubusercontent.com/lox/lifecycled/v1.0.0/init/upstart/lifecycled.conf
+	https://raw.githubusercontent.com/lox/lifecycled/v1.1.0/init/upstart/lifecycled.conf
 
 cat << EOF | sudo tee /usr/bin/buildkite-lifecycle-handler
 #!/bin/sh -eu
-stop buildkite-agent
-sleep 25
+echo "stopping buildkite-agent gracefully"
+service buildkite-agent stop
+while pgrep buildkite-agent > /dev/null; do
+  sleep 0.5
+done
+echo "buildkite-agent stopped!"
 EOF
 
 sudo chmod +x /usr/bin/buildkite-lifecycle-handler
