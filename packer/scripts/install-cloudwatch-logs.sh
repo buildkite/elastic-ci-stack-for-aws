ae5c4d1b5ee49c41cb88254e87114e4cd1ed4f41
Lachlan Donald <lachlan@ljd.cc>
2016-05-10 16:48:04 +1000

    Add minimal cloudwatch logs setup

---
+++ packer/scripts/install-cloudwatch-logs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -eu
+
+sudo yum install -y awslogs
+sudo chkconfig awslogs on
+
+cat << EOF | sudo tee /etc/awslogs/awslogs.conf
+[general]
+state_file = /var/awslogs/state/agent-state
+
+[/var/log/messages]
+file = /var/log/messages
+log_group_name = /var/log/messages
+log_stream_name = {instance_id}
+datetime_format = %b %d %H:%M:%S
+EOF
