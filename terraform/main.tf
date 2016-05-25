6332383486f2cbe970f6a10aa69a165130a9f461
Marty Zalega <marty@zalega.me>
2016-05-25 20:31:27 +1000

    Include terraform to provision stack

---
+++ terraform/main.tf
@@ -0,0 +1,45 @@
+provider "aws" {
+
+}
+
+resource "aws_s3_bucket" "buildkite_artifacts" {
+  bucket = "${var.name}-artifacts"
+  acl = "private"
+  force_destroy = true
+
+  lifecycle_rule {
+    prefix = "*"
+    enabled = true
+
+    expiration {
+      days = "${var.artifact_retention}"
+    }
+  }
+}
+
+resource "aws_s3_bucket" "buildkite_secrets" {
+  bucket = "${var.name}-secrets"
+  acl = "private"
+  force_destroy = true
+}
+
+resource "aws_cloudformation_stack" "buildkite" {
+  name = "${var.name}Stack"
+  template_url = "https://s3.amazonaws.com/buildkite-aws-stack/aws-stack.json"
+  capabilities = ["CAPABILITY_IAM"]
+
+  parameters {
+    KeyName = "${var.key_name}"
+    BuildkiteOrgSlug = "${var.buildkite_org_slug}"
+    BuildkiteAgentToken = "${var.buildkite_agent_token}"
+    BuildkiteApiAccessToken = "${var.buildkite_api_access_token}"
+    SecretsBucket = "${aws_s3_bucket.buildkite_secrets.id}"
+    ArtifactsBucket = "${aws_s3_bucket.buildkite_artifacts.id}"
+    InstanceType = "${var.instance_type}"
+    MaxSize = "${var.max_size}"
+    MinSize = "${var.min_size}"
+    RootVolumeSize = "${var.volume_size}"
+    ImageId = "${var.image_id}"
+  }
+}
+
---
+++ terraform/variables.tf
@@ -0,0 +1,51 @@
+variable "name" {
+  description = "Name of the stack"
+  name = "buildkite"
+}
+
+variable "image_id" {
+  description = "The AMI to use"
+  default = "ami-1dc83e70"
+}
+
+variable "key_name" {
+  description = "The ssh keypair used to access the buildkite instances"
+}
+
+variable "buildkite_org_slug" {
+  description = "Your Buildkite organization's slug"
+}
+
+variable "buildkite_agent_token" {
+  description = "Your Buildkite organization's agent token"
+}
+
+variable "buildkite_api_access_token" {
+  description = "A Buildkite API access token (with read_pipelines, read_builds and read_agents) used for gathering metrics"
+}
+
+variable "instance_type" {
+  description = "The type of instance to use for the agent"
+  default = "t2.nano"
+}
+
+variable "max_size" {
+  description = "The maximum number of agents to launch"
+  default = 10
+}
+
+variable "min_size" {
+  description = "The minumum number of agents to launch"
+  default = 0
+}
+
+variable "volume_size" {
+  description = "Size of EBS volume for root filesystem in GB"
+  default = 250
+}
+
+variable "artifact_retention" {
+  description = "Number of days to retain artifacts"
+  default = 9
+}
+
