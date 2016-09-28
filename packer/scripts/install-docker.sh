73f03b026ddc65f950044ed01880dc635d53a65a
Michael Clay <michaelc@dubsmash.com>
2016-09-28 16:43:42 +0200

    Bumping docker-compose to 1.8.1

--- packer/scripts/install-docker.sh
+++ packer/scripts/install-docker.sh
@@ -31,7 +31,7 @@ sudo cp /tmp/conf/docker/docker.conf /etc/sysconfig/docker
 sudo chkconfig docker on
 
 echo "Downloading docker-compose..."
-sudo curl -Lsf -o /usr/bin/docker-compose https://github.com/docker/compose/releases/download/1.8.0/docker-compose-Linux-x86_64
+sudo curl -Lsf -o /usr/bin/docker-compose https://github.com/docker/compose/releases/download/1.8.1/docker-compose-Linux-x86_64
 sudo chmod +x /usr/bin/docker-compose
 docker-compose --version
 
