6afbe5bf9ca61851fe3403b8c293b77043bf8cdf
Lachlan Donald <lachlan@ljd.cc>
2016-12-20 12:20:58 +1100

    Use install-utils script to install bk bins

--- packer/scripts/install-utils.sh
+++ packer/scripts/install-utils.sh
@@ -5,3 +5,7 @@ set -eu -o pipefail
 echo "Installing zip utils..."
 sudo yum update -y -q
 sudo yum install -y zip unzip
+
+echo "Installing bk elastic stack bin files"
+chod +x /tmp/bin/bk-*
+mv /tmp/bin/bk-* /usr/local/bin
