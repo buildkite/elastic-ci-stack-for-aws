5b237d0c273bbb84ae509822056ab9fa6c29d260
Lachlan Donald <lachlan@ljd.cc>
2016-12-20 12:34:14 +1100

    Fix incorrect paths for bins

--- packer/scripts/install-utils.sh
+++ packer/scripts/install-utils.sh
@@ -7,6 +7,5 @@ sudo yum update -y -q
 sudo yum install -y zip unzip
 
 echo "Installing bk elastic stack bin files"
-ls -alR /tmp
-chmod +x /tmp/bin/bk-*
-mv /tmp/bin/bk-* /usr/local/bin
+chmod +x /tmp/conf/bin/bk-*
+mv /tmp/conf/bin/bk-* /usr/local/bin
