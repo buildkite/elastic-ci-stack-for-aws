8419ea53a6abb77f4adb7c5da4cc96344ed01be8
Lachlan Donald <lachlan@ljd.cc>
2016-12-20 12:39:21 +1100

    Add sudo to bin installs

--- packer/scripts/install-utils.sh
+++ packer/scripts/install-utils.sh
@@ -7,5 +7,5 @@ sudo yum update -y -q
 sudo yum install -y zip unzip
 
 echo "Installing bk elastic stack bin files"
-chmod +x /tmp/conf/bin/bk-*
-mv /tmp/conf/bin/bk-* /usr/local/bin
+sudo chmod +x /tmp/conf/bin/bk-*
+sudo mv /tmp/conf/bin/bk-* /usr/local/bin
