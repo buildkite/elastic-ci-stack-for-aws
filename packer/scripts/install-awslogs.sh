4af25289e9e4046a7c6a9fbb042a95a57a803436
Lachlan Donald <lachlan@ljd.cc>
2016-12-28 17:45:23 +1100

    Bump amazon linux to 2016.09.1

--- packer/scripts/install-awslogs.sh
+++ packer/scripts/install-awslogs.sh
@@ -7,10 +7,8 @@ sudo yum update -y -q
 sudo yum install -y awslogs
 
 echo "Adding awslogs config..."
-
 sudo mkdir -p /var/awslogs/state
 sudo cp /tmp/conf/awslogs/awslogs.conf /etc/awslogs/awslogs.conf
 
 echo "Configure it to run on startup..."
-
 sudo chkconfig awslogs on
