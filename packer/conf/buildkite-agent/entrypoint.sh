9bd6205c5afc702e7ebfd605771519a8d01e4052
Lachlan Donald <lachlan@ljd.cc>
2017-02-19 19:53:11 +1100

    Install bats in the image if no there

--- packer/conf/buildkite-agent/entrypoint.sh
+++ packer/conf/buildkite-agent/entrypoint.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-command -v aws || apk --update awscli
+command -v bats || (
+  curl -O https://ghostbar.github.io/alpine-pkg-bats/v3.2/pkgs/x86_64/bats-0.4.0-r0.apk
+  apk add --allow-untrusted bats-0.4.0-r0.apk
+)
+
+command -v aws || (
+  apk --update awscli
+)
