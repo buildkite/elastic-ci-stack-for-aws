7edfba6bfd7fc5a0a45db86b2b4bceceac84763c
Lachlan Donald <lachlan@ljd.cc>
2017-02-19 13:16:19 +1100

    Use docker images for agents

--- packer/conf/bin/bk-install-elastic-stack.sh
+++ packer/conf/bin/bk-install-elastic-stack.sh
@@ -34,54 +34,23 @@ cwlogs = cwlogs
 region = $AWS_REGION
 EOF
 
-# cfn-env is sourced by the environment hook in builds
-cat << EOF > /var/lib/buildkite-agent/cfn-env
-BUILDKITE_STACK_NAME=$BUILDKITE_STACK_NAME
-BUILDKITE_SECRETS_BUCKET=$BUILDKITE_SECRETS_BUCKET
-BUILDKITE_AGENTS_PER_INSTANCE=$BUILDKITE_AGENTS_PER_INSTANCE
+# .env is read by docker-compose
+cat << EOF > /var/lib/buildkite-agent/.env
 AWS_DEFAULT_REGION=$AWS_REGION
 AWS_REGION=$AWS_REGION
+BUILDKITE_STACK_NAME=$BUILDKITE_STACK_NAME
+BUILDKITE_SECRETS_BUCKET=$BUILDKITE_SECRETS_BUCKET
+BUILDKITE_AGENT_RELEASE=$BUILDKITE_AGENT_RELEASE
+BUILDKITE_AGENTS_PER_INSTANCE=$BUILDKITE_AGENTS_PER_INSTANCE
+BUILDKITE_AGENT_ACCESS_TOKEN=${BUILDKITE_AGENT_TOKEN}
+BUILDKITE_AGENT_NAME=${BUILDKITE_STACK_NAME}-${INSTANCE_ID}-%n
+BUILDKITE_AGENT_META_DATA=$(printf 'queue=%s,docker=%s,stack=%s,buildkite-aws-stack=%s' "${BUILDKITE_QUEUE}" "${DOCKER_VERSION}" "${BUILDKITE_STACK_NAME}" "${BUILDKITE_STACK_VERSION}")
+BUILDKITE_AGENT_META_DATA_EC2=true
 EOF
 
 if [[ "${BUILDKITE_ECR_POLICY:-none}" != "none" ]] ; then
-	printf "AWS_ECR_LOGIN=1\n" >> /var/lib/buildkite-agent/cfn-env
+	printf "AWS_ECR_LOGIN=1\n" >> /var/lib/buildkite-agent/.env
 fi
-
-# Choose the right agent binary
-ln -s "/usr/bin/buildkite-agent-${BUILDKITE_AGENT_RELEASE}" /usr/bin/buildkite-agent
-
-# Once 3.0 is stable we can just remove this and let the agent do the right thing
-if [[ "${BUILDKITE_AGENT_RELEASE}" == "stable" ]]; then
-	BOOTSTRAP_SCRIPT="/etc/buildkite-agent/bootstrap.sh"
-else
-	BOOTSTRAP_SCRIPT="buildkite-agent bootstrap"
-fi;
-
-cat << EOF > /etc/buildkite-agent/buildkite-agent.cfg
-name="${BUILDKITE_STACK_NAME}-${INSTANCE_ID}-%n"
-token="${BUILDKITE_AGENT_TOKEN}"
-meta-data=$(printf 'queue=%s,docker=%s,stack=%s,buildkite-aws-stack=%s' "${BUILDKITE_QUEUE}" "${DOCKER_VERSION}" "${BUILDKITE_STACK_NAME}" "${BUILDKITE_STACK_VERSION}")
-meta-data-ec2=true
-bootstrap-script="${BOOTSTRAP_SCRIPT}"
-hooks-path=/etc/buildkite-agent/hooks
-build-path=/var/lib/buildkite-agent/builds
-plugins-path=/var/lib/buildkite-agent/plugins
-EOF
-
-chown buildkite-agent: /etc/buildkite-agent/buildkite-agent.cfg
-
-for i in $(seq 1 "${BUILDKITE_AGENTS_PER_INSTANCE}"); do
-	touch "/var/log/buildkite-agent-${i}.log"
-
-	# Setup logging first so we capture everything
-	cat <<- EOF > "/etc/awslogs/config/buildkite-agent-${i}.conf"
-	[/var/log/buildkite-agent-${i}.log]
-	file = /var/log/buildkite-agent-${i}.log
-	log_group_name = /var/log/buildkite-agent.log
-	log_stream_name = {instance_id}-${i}
-	datetime_format = %Y-%m-%d %H:%M:%S
-	EOF
-done
 
 if [[ -n "${BUILDKITE_AUTHORIZED_USERS_URL}" ]] ; then
 	cat <<- EOF > /etc/cron.hourly/authorized_keys
@@ -111,11 +80,8 @@ until docker ps || [ $next_wait_time -eq 5 ]; do
    sleep $(( next_wait_time++ ))
 done
 
-for i in $(seq 1 "${BUILDKITE_AGENTS_PER_INSTANCE}"); do
-	cp /etc/buildkite-agent/init.d.tmpl "/etc/init.d/buildkite-agent-${i}"
-	service "buildkite-agent-${i}" start
-	chkconfig --add "buildkite-agent-${i}"
-done
+docker-compose -f /var/lib/buildkite-agent/docker-compose.yml up -d
+docker-compose -f /var/lib/buildkite-agent/docker-compose.yml scale "agent=$BUILDKITE_AGENTS_PER_INSTANCE"
 
 /opt/aws/bin/cfn-signal \
 	--region "$AWS_REGION" \
--- packer/scripts/install-buildkite-agent.sh
+++ packer/scripts/install-buildkite-agent.sh
@@ -10,37 +10,12 @@ echo "Creating buildkite-agent user..."
 sudo useradd --base-dir /var/lib buildkite-agent
 sudo usermod -a -G docker buildkite-agent
 
-echo "Downloading buildkite-agent stable..."
-sudo curl -Lsf -o /usr/bin/buildkite-agent-stable \
-  "https://download.buildkite.com/agent/stable/latest/buildkite-agent-linux-amd64"
-sudo chmod +x /usr/bin/buildkite-agent-stable
-buildkite-agent-stable --version
+echo "Adding docker-compose.yml for buildkite..."
+sudo cp /tmp/conf/buildkite-agent/docker-compose.yml /var/lib/buildkite-agent/
+sudo chown buildkite-agent: /var/lib/buildkite-agent/docker-compose.yml
 
-echo "Downloading buildkite-agent beta..."
-sudo curl -Lsf -o /usr/bin/buildkite-agent-beta \
-  "https://download.buildkite.com/agent/unstable/latest/buildkite-agent-linux-amd64"
-sudo chmod +x /usr/bin/buildkite-agent-beta
-buildkite-agent-beta --version
-
-echo "Downloading buildkite-agent edge..."
-sudo curl -Lsf -o /usr/bin/buildkite-agent-edge \
-  "https://download.buildkite.com/agent/experimental/latest/buildkite-agent-linux-amd64"
-sudo chmod +x /usr/bin/buildkite-agent-edge
-buildkite-agent-edge --version
-
-echo "Downloading legacy bootstrap.sh for v2 stable agent..."
-sudo mkdir -p /etc/buildkite-agent
-sudo curl -Lsf -o /etc/buildkite-agent/bootstrap.sh \
-  https://raw.githubusercontent.com/buildkite/agent/2-1-stable/templates/bootstrap.sh
-sudo chmod +x /etc/buildkite-agent/bootstrap.sh
-sudo chown -R buildkite-agent: /etc/buildkite-agent
-
-echo "Adding scripts..."
-sudo cp /tmp/conf/buildkite-agent/scripts/* /usr/bin
-
-echo "Adding sudoers config..."
-sudo cp /tmp/conf/buildkite-agent/sudoers.conf /etc/sudoers.d/buildkite-agent
-sudo chmod 440 /etc/sudoers.d/buildkite-agent
+echo "Pulling buildkite-agent images..."
+sudo docker-compose -f /var/lib/buildkite-agent/docker-compose.yml pull
 
 echo "Creating hooks dir..."
 sudo mkdir -p /etc/buildkite-agent/hooks
@@ -50,17 +25,6 @@ echo "Copying custom hooks..."
 sudo cp -a /tmp/conf/buildkite-agent/hooks/* /etc/buildkite-agent/hooks
 sudo chmod +x /etc/buildkite-agent/hooks/*
 sudo chown -R buildkite-agent: /etc/buildkite-agent/hooks
-
-echo "Creating builds dir..."
-sudo mkdir -p /var/lib/buildkite-agent/builds
-sudo chown -R buildkite-agent: /var/lib/buildkite-agent/builds
-
-echo "Creating plugins dir..."
-sudo mkdir -p /var/lib/buildkite-agent/plugins
-sudo chown -R buildkite-agent: /var/lib/buildkite-agent/plugins
-
-echo "Adding init.d template..."
-sudo cp /tmp/conf/buildkite-agent/init.d/buildkite-agent /etc/buildkite-agent/init.d.tmpl
 
 echo "Adding termationd hook..."
 sudo cp /tmp/conf/buildkite-agent/terminationd/hook /etc/terminationd/hook
