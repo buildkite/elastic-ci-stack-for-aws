4a7d9dfe0d56c8c20dfac99e79f58c4fe458ea10
Lachlan Donald <lachlan@ljd.cc>
2017-02-21 21:12:51 +1100

    Remove bats and bats tests

--- packer/conf/buildkite-agent/entrypoint.sh
+++ packer/conf/buildkite-agent/entrypoint.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 set -euxo pipefail
 
-command -v bats || (
-  apk add bats \
-    --no-cache \
-    --repository http://dl-3.alpinelinux.org/alpine/edge/testing/ \
-    --allow-untrusted
-)
-
 command -v aws || (
   pip install awscli
 )
--- tests/docker.bats
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bats
-
-@test "Check docker is running" {
-	run docker info
-	[ $status = 0 ]
-}
