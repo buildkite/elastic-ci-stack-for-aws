cb1812ed9bac74948d9f74f99bda5281a9ef2312
Lachlan Donald <lachlan@ljd.cc>
2017-02-21 14:33:40 +1100

    Add bats via apk, custom didn't seem to work

--- packer/conf/buildkite-agent/entrypoint.sh
+++ packer/conf/buildkite-agent/entrypoint.sh
@@ -2,8 +2,10 @@
 set -euxo pipefail
 
 command -v bats || (
-  curl -O https://ghostbar.github.io/alpine-pkg-bats/v3.2/pkgs/x86_64/bats-0.4.0-r0.apk
-  apk add --allow-untrusted bats-0.4.0-r0.apk
+  apk add bats \
+    --no-cache \
+    --repository http://dl-3.alpinelinux.org/alpine/edge/testing/ \
+    --allow-untrusted
 )
 
 command -v aws || (
