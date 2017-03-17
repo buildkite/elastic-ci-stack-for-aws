2ab5a3d40a7973a41f718d86a3e265258cb4c51d
Lachlan Donald <lachlan@ljd.cc>
2017-03-17 17:12:44 +1100

    Revert "Bump amazon linux to 2016.09.1"
    This reverts commit 4af25289e9e4046a7c6a9fbb042a95a57a803436.

--- packer/scripts/install-awslogs.sh
+++ packer/scripts/install-awslogs.sh
@@ -7,8 +7,10 @@ sudo yum update -y -q
 sudo yum install -y awslogs
 
 echo "Adding awslogs config..."
+
 sudo mkdir -p /var/awslogs/state
 sudo cp /tmp/conf/awslogs/awslogs.conf /etc/awslogs/awslogs.conf
 
 echo "Configure it to run on startup..."
+
 sudo chkconfig awslogs on
