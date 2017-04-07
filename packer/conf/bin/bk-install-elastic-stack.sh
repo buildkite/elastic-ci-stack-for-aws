9b0990d2cbf6202b4cab17e603eca81d56401ae0
Lachlan Donald <lachlan@ljd.cc>
2017-04-07 11:32:47 +1000

    Add debugging on boot

--- packer/conf/bin/bk-install-elastic-stack.sh
+++ packer/conf/bin/bk-install-elastic-stack.sh
@@ -102,9 +102,12 @@ start terminationd || true
 service awslogs restart || true
 
 # start up docker, wait for it to initialize
-service docker start
-chkconfig docker on
-docker ps
+# service docker start
+# chkconfig docker on
+# docker ps
+
+service --status-all
+chkconfig --list
 
 for i in $(seq 1 "${BUILDKITE_AGENTS_PER_INSTANCE}"); do
 	cp /etc/buildkite-agent/init.d.tmpl "/etc/init.d/buildkite-agent-${i}"
