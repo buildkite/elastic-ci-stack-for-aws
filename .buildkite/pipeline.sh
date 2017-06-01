7fceb9fce2fa17fd64fe081ce3e83f7de5529ced
Lachlan Donald <lachlan@ljd.cc>
2017-06-01 14:54:33 +1000

    Target the same queue in the pipeline

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -8,21 +8,21 @@ steps:
   - command: .buildkite/steps/lint.sh
     name: "Run linting on shell scripts"
     agents:
-      queue: aws-stack
+      queue: "${BUILDKITE_AGENT_META_DATA_QUEUE}"
 
   - wait
 
   - command: .buildkite/steps/packer.sh
     name: "Build packer image"
     agents:
-      queue: aws-stack
+      queue: "${BUILDKITE_AGENT_META_DATA_QUEUE}"
 
   - wait
 
   - command: .buildkite/steps/test.sh
     name: "Launch :cloudformation: stack"
     agents:
-      queue: aws-stack
+      queue: "${BUILDKITE_AGENT_META_DATA_QUEUE}"
     artifact_paths:
       - "build/*.json"
       - "build/*.yml"
@@ -43,7 +43,7 @@ steps:
   - command: .buildkite/steps/publish.sh
     name: "Publishing :cloudformation: stack"
     agents:
-      queue: aws-stack
+      queue: "${BUILDKITE_AGENT_META_DATA_QUEUE}"
     artifact_paths: "templates/mappings.yml;build/aws-stack.json"
     concurrency_group: "aws-stack-publish"
     concurrency: 1
@@ -53,7 +53,7 @@ steps:
   - command: .buildkite/steps/cleanup.sh
     name: "Cleanup"
     agents:
-      queue: aws-stack
+      queue: "${BUILDKITE_AGENT_META_DATA_QUEUE}"
 EOF
 
 buildkite-agent meta-data set stack_name "$stack_name"
