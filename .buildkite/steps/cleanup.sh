37600f4ff7f0c53a3fae91dd96df0b64cad287af
Lachlan Donald <lachlan@ljd.cc>
2017-06-17 21:57:31 +1000

    Delete old managed secrets bucket from tests

--- .buildkite/steps/cleanup.sh
+++ .buildkite/steps/cleanup.sh
@@ -5,3 +5,11 @@ stack_name=$(buildkite-agent meta-data get stack_name)
 
 echo "--- Deleting stack $stack_name"
 aws cloudformation delete-stack --stack-name "$stack_name"
+
+echo "--- Deleting old managed secrets buckets"
+aws s3api list-buckets \
+  --output text \
+  --query "$(printf 'Buckets[?starts_with(Name, `%s`) == `true`][?CreationDate<`%s`].Name' \
+    "buildkite-aws-stack-test-managedsecrets" \
+    "$(date --date='-2 days' +%Y-%m-%d)" \
+  )"
