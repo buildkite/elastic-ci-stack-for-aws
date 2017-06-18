350a671b1047e91bb8ef6a21f734baa71a4df25e
Lachlan Donald <lachlan@ljd.cc>
2017-06-18 11:34:13 +1000

    Move cleanup code to another branch

--- .buildkite/steps/cleanup.sh
+++ .buildkite/steps/cleanup.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
-# shellcheck disable=SC2016
 set -eu
 
 stack_name=$(buildkite-agent meta-data get stack_name)
 
 echo "--- Deleting stack $stack_name"
 aws cloudformation delete-stack --stack-name "$stack_name"
-
-echo "--- Deleting old managed secrets buckets"
-aws s3api list-buckets \
-  --output text \
-  --query "$(printf 'Buckets[?starts_with(Name, `%s`) == `true`][?CreationDate<`%s`].Name' \
-    "buildkite-aws-stack-test-managedsecrets" \
-    "$(date --date='-2 days' +%Y-%m-%d)" \
-  )"
