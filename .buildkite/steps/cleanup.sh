8773fdb0aa30e2802d5a94aeee1cf4c06927bf9d
Lachlan Donald <lachlan@ljd.cc>
2017-06-18 13:19:27 +1000

    Filter down to only ci related buckets

--- .buildkite/steps/cleanup.sh
+++ .buildkite/steps/cleanup.sh
@@ -10,9 +10,11 @@ aws cloudformation delete-stack --stack-name "$AWS_STACK_NAME"
 echo "--- Deleting test managed secrets buckets created"
 aws s3api list-buckets \
   --output text \
-  --query "$(printf '[?CreationDate<`%s`].Name' "$cutoff_date" )"
+  --query "$(printf '[?CreationDate<`%s`].Name' "$cutoff_date" )" |
+  grep -E 'buildkite-aws-stack-test(-\d+)?-managed'
 
 echo "--- Deleting old cloudformation stacks"
 aws cloudformation describe-stacks \
   --output text \
-  --query "$(printf 'Stacks[?LastUpdateTime<`%s`].StackName' "$cutoff_date" )"
+  --query "$(printf 'Stacks[?CreationTime<`%s`].StackName' "$cutoff_date" )" |
+  grep -E 'buildkite-aws-stack-test-(\d+)'
