f4fffe58a3c32c5309f87dbcaa9115b7a286a00b
Lachlan Donald <lachlan@ljd.cc>
2017-06-30 16:40:49 +1000

    Enable shudder init and write config

--- packer/scripts/install-shudder.sh
+++ packer/scripts/install-shudder.sh
@@ -2,8 +2,6 @@
 
 set -eu -o pipefail
 
-pip install shudder==0.1.0
-
+sudo pip install shudder==0.1.0
 sudo mkdir /etc/shudder
-sudo cp /tmp/conf/shudder/conf/* /etc/shudder
 sudo cp /tmp/conf/shudder/upstart/* /etc/init
