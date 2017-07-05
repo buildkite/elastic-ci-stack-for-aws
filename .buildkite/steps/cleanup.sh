5466f4b9ae77be61d2dfdd75341f778eb24defb5
Lachlan Donald <lachlan@ljd.cc>
2017-07-05 19:18:10 +1000

    Split lines with xargs

--- .buildkite/steps/cleanup.sh
+++ .buildkite/steps/cleanup.sh
@@ -10,11 +10,15 @@ aws cloudformation delete-stack --stack-name "$AWS_STACK_NAME"
 echo "--- Deleting test managed secrets buckets created"
 aws s3api list-buckets \
   --output text \
-  --query "$(printf '[?CreationDate<`%s`].Name' "$cutoff_date" )" |
-  grep -E 'buildkite-aws-stack-test(-\d+)?-managed' || true
+  --query "$(printf '[?CreationDate<`%s`].Name' "$cutoff_date" )" \
+  | xargs -n1 \
+  | grep -E 'buildkite-aws-stack-test(-\d+)?-managed' \
+  | xargs -n1 echo rm || true
 
 echo "--- Deleting old cloudformation stacks"
 aws cloudformation describe-stacks \
   --output text \
-  --query "$(printf 'Stacks[?CreationTime<`%s`].StackName' "$cutoff_date" )" |
-  grep -E 'buildkite-aws-stack-test-(\d+)' || true
+  --query "$(printf 'Stacks[?CreationTime<`%s`].StackName' "$cutoff_date" )" \
+  | xargs -n1 \
+  | grep -E 'buildkite-aws-stack-test-\d+' \
+  | xargs -n1 echo rm || true
