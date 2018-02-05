5cf15f57f051386d9cffe53b6af485d26d752a06
Lachlan Donald <lachlan@ljd.cc>
2018-02-06 09:28:13 +1100

    Trim leading whitespace from disk space output 💅🏻

--- packer/conf/bin/bk-check-disk-space.sh
+++ packer/conf/bin/bk-check-disk-space.sh
@@ -6,7 +6,7 @@ DISK_MIN_INODES=${DISK_MIN_INODES:-10000}
 
 disk_avail=$(df -k --output=avail "$PWD" | tail -n1)
 
-echo "Disk space free: $(df -k -h --output=avail "$PWD" | tail -n1)"
+echo "Disk space free: $(df -k -h --output=avail "$PWD" | tail -n1 | sed -e 's/^[[:space:]]//')"
 
 if [[ $disk_avail -lt $DISK_MIN_AVAILABLE ]]; then
   echo "Not enough disk space free, cutoff is ${DISK_MIN_AVAILABLE} 🚨" >&2
@@ -15,7 +15,7 @@ fi
 
 inodes_avail=$(df -k --output=iavail "$PWD" | tail -n1)
 
-echo "Inodes free: $(df -k -h --output=iavail "$PWD" | tail -n1)"
+echo "Inodes free: $(df -k -h --output=iavail "$PWD" | tail -n1 | sed -e 's/^[[:space:]]//')"
 
 if [[ $inodes_avail -lt $DISK_MIN_INODES ]]; then
   echo "Not enough inodes free, cutoff is ${DISK_MIN_INODES inodes} 🚨" >&2
