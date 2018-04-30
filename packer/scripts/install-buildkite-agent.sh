0a02d56491fbeef213a73821405b495b815e5113
Lachlan Donald <lachlan@ljd.cc>
2018-04-30 20:40:09 +1000

    Use uid/gid of 2000 for buildkite-agent to avoid conflict

--- packer/scripts/install-buildkite-agent.sh
+++ packer/scripts/install-buildkite-agent.sh
@@ -7,7 +7,7 @@ sudo yum update -y -q
 sudo yum install -y -q git-core
 
 echo "Creating buildkite-agent user..."
-sudo useradd --base-dir /var/lib -u 1000 -g 1000 buildkite-agent
+sudo useradd --base-dir /var/lib -u 2000 -g 2000 buildkite-agent
 sudo usermod -a -G docker buildkite-agent
 
 echo "Downloading buildkite-agent stable..."
