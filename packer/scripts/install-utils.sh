d1d35c53c21c2a577a87a42f5fc9937d0f57466c
Lachlan Donald <lachlan@ljd.cc>
2018-08-14 13:35:09 +1000

    Install Git LFS
    Closes #467

--- packer/scripts/install-utils.sh
+++ packer/scripts/install-utils.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -eu -o pipefail
 
+GIT_LFS_RELEASE="2.4.0"
+
 echo "Updating awscli..."
 sudo yum update -y awscli
 
@@ -19,3 +21,12 @@ sudo mv /tmp/conf/bin/bk-* /usr/local/bin
 
 echo "Configuring awscli to use v4 signatures..."
 sudo aws configure set s3.signature_version s3v4
+
+echo "Installing git lfs..."
+curl -Lsf -o git-lfs.tgz https://github.com/git-lfs/git-lfs/archive/v${GIT_LFS_RELEASE}.tar.gz
+tar -xvzf git-lfs.tgz
+cd git-lfs-${GIT_LFS_RELEASE}/
+sudo ./script/bootstrap
+sudo mv bin/git-lfs /usr/bin
+sudo git lfs install
+rm git-lfs.tgz
