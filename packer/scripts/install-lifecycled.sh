1155cb55f30294deb8f5f7a156f76e9673ed6b69
Lachlan Donald <lachlan@buildkite.com>
2018-12-16 19:22:41 +1100

    Only start lifecycled after install

--- packer/scripts/install-lifecycled.sh
+++ packer/scripts/install-lifecycled.sh
@@ -12,5 +12,3 @@ sudo chmod +x /usr/bin/lifecycled
 sudo curl -Lf -o /etc/systemd/system/lifecycled.service \
 	https://raw.githubusercontent.com/lox/lifecycled/${LIFECYCLED_VERSION}/init/systemd/lifecycled.unit
 
-echo "Configure lifecycled to run on startup..."
-sudo systemctl enable lifecycled.service
