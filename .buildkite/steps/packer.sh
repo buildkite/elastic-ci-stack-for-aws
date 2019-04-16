0dc221ec44ab702b89e97e0c0da19116e7e32a2f
Lachlan Donald <lachlan@buildkite.com>
2019-04-16 15:20:19 +1000

    Support building windows packer images

--- .buildkite/steps/packer.sh
+++ .buildkite/steps/packer.sh
@@ -6,15 +6,18 @@ if [[ -z "${BUILDKITE_AWS_STACK_BUCKET}" ]] ; then
   exit 1
 fi
 
-mkdir -p build
+os="${1:-linux}"
+build_dir="build/${os}"
+
+mkdir -p "${build_dir}"
 
 # Build a hash of packer files and the agent versions
-packer_files_sha=$(find packer/ plugins/ -type f -print0 | xargs -0 sha1sum | awk '{print $1}' | sort | sha1sum | awk '{print $1}')
-stable_agent_sha=$(curl -Lfs https://download.buildkite.com/agent/stable/latest/buildkite-agent-linux-amd64.sha256)
-unstable_agent_sha=$(curl -Lfs https://download.buildkite.com/agent/unstable/latest/buildkite-agent-linux-amd64.sha256)
+packer_files_sha=$(find "packer/${os}" plugins/ -type f -print0 | xargs -0 sha1sum | awk '{print $1}' | sort | sha1sum | awk '{print $1}')
+stable_agent_sha=$(curl -Lfs https://download.buildkite.com/agent/stable/latest/buildkite-agent-${os}-amd64.sha256)
+unstable_agent_sha=$(curl -Lfs https://download.buildkite.com/agent/unstable/latest/buildkite-agent-${os}-amd64.sha256)
 packer_hash=$(echo "$packer_files_sha" "$stable_agent_sha" "$unstable_agent_sha" | sha1sum | awk '{print $1}')
 
-echo "Packer image hash is $packer_hash"
+echo "Packer image hash for ${os} is ${packer_hash}"
 packer_file="${packer_hash}.packer"
 
 # Only build packer image if one with the same hash doesn't exist
