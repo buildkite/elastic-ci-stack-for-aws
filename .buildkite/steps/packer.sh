ee1db30ce9c98649ecfb7427270f830b4e9dad12
Lachlan Donald <lachlan@buildkite.com>
2019-04-16 15:35:08 +1000

    Refer to the correct windows binary

--- .buildkite/steps/packer.sh
+++ .buildkite/steps/packer.sh
@@ -8,13 +8,18 @@ fi
 
 os="${1:-linux}"
 build_dir="build/${os}"
+agent_binary="buildkite-agent-${os}-amd64"
+
+if [[ "$os" == "windows" ]] ; then
+  binary_name+=".exe"
+fi
 
 mkdir -p "${build_dir}"
 
 # Build a hash of packer files and the agent versions
 packer_files_sha=$(find "packer/${os}" plugins/ -type f -print0 | xargs -0 sha1sum | awk '{print $1}' | sort | sha1sum | awk '{print $1}')
-stable_agent_sha=$(curl -Lfs "https://download.buildkite.com/agent/stable/latest/buildkite-agent-${os}-amd64.sha256")
-unstable_agent_sha=$(curl -Lfs "https://download.buildkite.com/agent/unstable/latest/buildkite-agent-${os}-amd64.sha256")
+stable_agent_sha=$(curl -Lfs "https://download.buildkite.com/agent/stable/latest/${agent_binary}.sha256")
+unstable_agent_sha=$(curl -Lfs "https://download.buildkite.com/agent/unstable/latest/${agent_binary}.sha256")
 packer_hash=$(echo "$packer_files_sha" "$stable_agent_sha" "$unstable_agent_sha" | sha1sum | awk '{print $1}')
 
 echo "Packer image hash for ${os} is ${packer_hash}"
@@ -33,4 +38,4 @@ fi
 image_id=$(grep -Eo "${AWS_REGION}: (ami-.+)$" "$packer_file" | awk '{print $2}')
 echo "AMI for ${AWS_REGION} is $image_id"
 
-buildkite-agent meta-data set image_id "${os}_$image_id"
+buildkite-agent meta-data set "${os}_image_id" "$image_id"
