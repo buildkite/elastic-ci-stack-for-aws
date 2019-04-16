61fc5b0dba4cf8f93872b77ae533fb423a1f588e
Lachlan Donald <lachlan@buildkite.com>
2019-04-16 15:23:02 +1000

    Fix linting issues

--- .buildkite/steps/packer.sh
+++ .buildkite/steps/packer.sh
@@ -13,8 +13,8 @@ mkdir -p "${build_dir}"
 
 # Build a hash of packer files and the agent versions
 packer_files_sha=$(find "packer/${os}" plugins/ -type f -print0 | xargs -0 sha1sum | awk '{print $1}' | sort | sha1sum | awk '{print $1}')
-stable_agent_sha=$(curl -Lfs https://download.buildkite.com/agent/stable/latest/buildkite-agent-${os}-amd64.sha256)
-unstable_agent_sha=$(curl -Lfs https://download.buildkite.com/agent/unstable/latest/buildkite-agent-${os}-amd64.sha256)
+stable_agent_sha=$(curl -Lfs "https://download.buildkite.com/agent/stable/latest/buildkite-agent-${os}-amd64.sha256")
+unstable_agent_sha=$(curl -Lfs "https://download.buildkite.com/agent/unstable/latest/buildkite-agent-${os}-amd64.sha256")
 packer_hash=$(echo "$packer_files_sha" "$stable_agent_sha" "$unstable_agent_sha" | sha1sum | awk '{print $1}')
 
 echo "Packer image hash for ${os} is ${packer_hash}"
