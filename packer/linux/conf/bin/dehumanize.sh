956ca6099ef27cf1c3a794a5356c69aae5787ff3
Lachlan Donald <lachlan@buildkite.com>
2019-10-07 13:34:56 +1100

    Move ad-hoc tests into bats tests

--- packer/linux/conf/bin/dehumanize.sh
+++ packer/linux/conf/bin/dehumanize.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
+if [[ ! "${1:-}" =~ ^[0-9] ]] ; then
+  printf "Invalid input, must start with a number: %q\n" "${1:-}" >&2
+  exit 1
+fi
+
 # Converts human-readable units like 1.43K and 120.3M to bytes
-dehumanize() {
-  awk '/[0-9][bB]?$/ {printf "%u\n", $1*1}
-       /[tT][bB]?$/  {printf "%u\n", $1*(1024*1024*1024*1024)}
-       /[gG][bB]?$/  {printf "%u\n", $1*(1024*1024*1024)}
-       /[mM][bB]?$/  {printf "%u\n", $1*(1024*1024)}
-       /[kK][bB]?$/  {printf "%u\n", $1*1024}' <<< "$1"
-}
+/usr/bin/awk '/[0-9][bB]?$/ {printf "%u\n", $1*1}
+     /[tT][bB]?$/  {printf "%u\n", $1*(1024*1024*1024*1024)}
+     /[gG][bB]?$/  {printf "%u\n", $1*(1024*1024*1024)}
+     /[mM][bB]?$/  {printf "%u\n", $1*(1024*1024)}
+     /[kK][bB]?$/  {printf "%u\n", $1*1024}' <<< "$1"
