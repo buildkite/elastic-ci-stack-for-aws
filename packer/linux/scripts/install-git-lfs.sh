44045f3a0a40eccf58708e665cf30bb1b6842902
Kush Mansingh <12158241+kushmansingh@users.noreply.github.com>
2020-02-21 09:54:56 -0500

    Upgrade buildkite-agent and git lfs versions

--- packer/linux/scripts/install-git-lfs.sh
+++ packer/linux/scripts/install-git-lfs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-GIT_LFS_RELEASE="2.5.1"
+GIT_LFS_RELEASE="2.10.0"
 
 echo "Installing git lfs ${GIT_LFS_RELEASE}..."
 curl -Lsf -o git-lfs.tgz https://github.com/git-lfs/git-lfs/releases/download/v${GIT_LFS_RELEASE}/git-lfs-linux-amd64-v${GIT_LFS_RELEASE}.tar.gz
