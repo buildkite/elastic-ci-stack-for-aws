56e7fab3d64f2401ecb64bd8d43594a90c7b7871
Keith Duncan <keith_duncan@me.com>
2020-04-20 19:35:14 +1000

    Fix the version url for iam-ssh-agent

--- packer/linux/scripts/install-iam-ssh-agent.sh
+++ packer/linux/scripts/install-iam-ssh-agent.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -eu -o pipefail
 
-IAM_SSH_AGENT_VERSION="v0.3.0"
+IAM_SSH_AGENT_VERSION="0.3.0"
 
 echo "Installing iam-ssh-agent ${IAM_SSH_AGENT_VERSION}..."
 
 sudo curl --location --fail --output /tmp/iam-ssh-agent.rpm \
-	"https://github.com/keithduncan/iam-ssh-agent/releases/download/${IAM_SSH_AGENT_VERSION}/iam-ssh-agent_${IAM_SSH_AGENT_VERSION}_amd64.rpm"
+	"https://github.com/keithduncan/iam-ssh-agent/releases/download/v${IAM_SSH_AGENT_VERSION}/iam-ssh-agent_${IAM_SSH_AGENT_VERSION}_amd64.rpm"
 
 sudo yum install --assumeyes /tmp/iam-ssh-agent.rpm
 
