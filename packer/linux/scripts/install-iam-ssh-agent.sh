d77cdb50d3ae56ac6a8df9a2696a0dd5dd15267b
Keith Duncan <keith_duncan@me.com>
2020-04-20 17:11:22 +1000

    Copy the iam-ssh-agent service to the buildkite-agent user directory instead of systemwide

--- packer/linux/scripts/install-iam-ssh-agent.sh
+++ packer/linux/scripts/install-iam-ssh-agent.sh
@@ -12,4 +12,4 @@ sudo yum install --assumeyes /tmp/iam-ssh-agent.rpm
 
 rm /tmp/iam-ssh-agent.rpm
 
-sudo cp /tmp/conf/iam-ssh-agent/systemd/iam-ssh-agent.service /etc/systemd/system/iam-ssh-agent.service
+sudo cp /tmp/conf/iam-ssh-agent/systemd/iam-ssh-agent.service /var/lib/buildkite-agent/.config/systemd/user/ssh-agent.service
