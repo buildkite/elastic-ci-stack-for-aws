22176e89b90a950171a40eb60d556b07add914ac
Chloe Hutchinson <chloe@buildkite.com>
2020-10-20 16:02:24 +1100

    Remove Stop-Computer directive – EC2 will do that for us

--- packer/windows/conf/buildkite-agent/scripts/terminate-instance.ps1
+++ packer/windows/conf/buildkite-agent/scripts/terminate-instance.ps1
@@ -1,9 +1,11 @@
 # Stop script execution when a non-terminating error occurs
 $ErrorActionPreference = "Stop"
 
+Write-Output "terminate-instance: waiting for 10 seconds to allow agent logs to drain to Cloudwatch..."
+Start-Sleep -Seconds 10
+
 $InstanceId = (Invoke-WebRequest -UseBasicParsing http://169.254.169.254/latest/meta-data/instance-id).content
 $Region = (Invoke-WebRequest -UseBasicParsing http://169.254.169.254/latest/meta-data/placement/availability-zone).content -replace ".$"
 
+Write-Output "terminate-instance: requesting instance termination..."
 aws autoscaling terminate-instance-in-auto-scaling-group --region "$Region" --instance-id "$InstanceId" "--should-decrement-desired-capacity"
-
-Stop-Computer $env:computername -Force
