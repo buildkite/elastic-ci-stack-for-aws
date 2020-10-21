2c15ca803b41fa74410bf784fd3c96acde5fc571
Chloe Hutchinson <chloe@buildkite.com>
2020-10-21 12:09:21 +1100

    Add nssm stop signal if terminating

--- packer/windows/conf/buildkite-agent/scripts/terminate-instance.ps1
+++ packer/windows/conf/buildkite-agent/scripts/terminate-instance.ps1
@@ -9,3 +9,11 @@ $Region = (Invoke-WebRequest -UseBasicParsing http://169.254.169.254/latest/meta
 
 Write-Output "terminate-instance: requesting instance termination..."
 aws autoscaling terminate-instance-in-auto-scaling-group --region "$Region" --instance-id "$InstanceId" "--should-decrement-desired-capacity"
+
+if ($lastexitcode -eq 0) { # If autoscaling request was successful, we will terminate
+  Write-Output "terminate-instance: disabling buildkite-agent service"
+  nssm stop buildkite-agent
+}
+else {
+  Write-Output "terminate-instance: ASG could not decrement (we're already at minSize)"
+}
