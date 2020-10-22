31182c787d2ef877084fa41761123032c80499d7
Chloe Hutchinson <chloe@buildkite.com>
2020-10-22 13:58:23 +1100

    Redirect stderr to null

--- packer/windows/conf/buildkite-agent/scripts/terminate-instance.ps1
+++ packer/windows/conf/buildkite-agent/scripts/terminate-instance.ps1
@@ -5,7 +5,7 @@ $InstanceId = (Invoke-WebRequest -UseBasicParsing http://169.254.169.254/latest/
 $Region = (Invoke-WebRequest -UseBasicParsing http://169.254.169.254/latest/meta-data/placement/availability-zone).content -replace ".$"
 
 Write-Output "terminate-instance: requesting instance termination..."
-aws autoscaling terminate-instance-in-auto-scaling-group --region "$Region" --instance-id "$InstanceId" "--should-decrement-desired-capacity"
+aws autoscaling terminate-instance-in-auto-scaling-group --region "$Region" --instance-id "$InstanceId" "--should-decrement-desired-capacity" 2> $null
 
 if ($lastexitcode -eq 0) { # If autoscaling request was successful, we will terminate
   Write-Output "terminate-instance: disabling buildkite-agent service"
@@ -13,5 +13,4 @@ if ($lastexitcode -eq 0) { # If autoscaling request was successful, we will term
 }
 else {
   Write-Output "terminate-instance: ASG could not decrement (we're already at minSize)"
-  nssm start buildkite-agent
 }
