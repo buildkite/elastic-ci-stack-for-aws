14a830417194f83dec7cfe56ffe7ce6de2dfc72b
Keith Duncan <keith.d@buildkite.com>
2021-07-28 20:33:14 +1000

    Fix curl error 23 write error downloading the binary

--- packer/linux/scripts/install-s3secrets-helper.sh
+++ packer/linux/scripts/install-s3secrets-helper.sh
@@ -11,6 +11,6 @@ case "${MACHINE}" in
 esac
 
 echo "Downloading s3-secrets-helper..."
-curl -Lsf -o /usr/local/bin/s3secrets-helper \
+sudo curl -Lsf -o /usr/local/bin/s3secrets-helper \
 	"https://github.com/buildkite/elastic-ci-stack-s3-secrets-hooks/releases/download/v${S3_SECRETS_HELPER_VERSION}/s3secrets-helper-linux-${ARCH}"
 sudo chmod +x /usr/local/bin/s3secrets-helper
