639eb41b0b3978f0dd07f6fa39469363dfa4aa01
Keith Duncan <keith.d@buildkite.com>
2021-07-28 20:59:07 +1000

    Put this in the right place

--- packer/windows/scripts/install-s3secrets-helper.ps1
+++ packer/windows/scripts/install-s3secrets-helper.ps1
@@ -5,5 +5,5 @@ $S3_SECRETS_HELPER_VERSION = "2.1.1"
 
 Write-Output "Downloading s3-secrets-helper v${S3_SECRETS_HELPER_VERSION}..."
 Invoke-WebRequest -OutFile C:\buildkite-agent\bin\s3secrets-helper.exe -Uri "https://github.com/buildkite/elastic-ci-stack-s3-secrets-hooks/releases/download/v${S3_SECRETS_HELPER_VERSION}/s3secrets-helper-windows-amd64"
-s3-secrets-helper.exe
+s3secrets-helper.exe
 If ($lastexitcode -ne 0) { Exit $lastexitcode }
