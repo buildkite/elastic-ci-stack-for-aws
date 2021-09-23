cbe0f334a4f01aae23078fb7b9c9c8f0658f3e47
Keith Duncan <keith_duncan@me.com>
2021-09-24 05:44:22 +1000

    Try running the deletes in parallel

--- .buildkite/steps/cleanup.sh
+++ .buildkite/steps/cleanup.sh
@@ -35,9 +35,10 @@ delete_service_role_stack() {
 }
 
 if [[ -n "${BUILDKITE_BUILD_NUMBER:-}" ]] ; then
-  delete_test_stack "windows-amd64"
-  delete_test_stack "linux-amd64"
-  delete_test_stack "linux-arm64"
+  delete_test_stack "windows-amd64" &
+  delete_test_stack "linux-amd64" &
+  delete_test_stack "linux-arm64" &
+  wait
 fi
 
 # Must run after all the test stacks that use it have been successfully removed
