1eec5ad25470cda426254233a76e2c06ce0eb1b5
Keith Duncan <keith.d@buildkite.com>
2021-09-23 21:54:43 +1000

    Use the stack name variable

--- .buildkite/steps/deploy-service-role-stack.sh
+++ .buildkite/steps/deploy-service-role-stack.sh
@@ -6,5 +6,5 @@ buildkite-agent meta-data set service-role-stack-name "${stack_name}"
 
 aws cloudformation deploy --template-file templates/service-role.yml --stack-name "${stack_name}" --region us-east-1 --capabilities CAPABILITY_IAM
 
-role_arn="$(aws cloudformation describe-stacks --stack-name buildkite-elastic-ci-stack-service-role --region us-east-1 --query "Stacks[0].Outputs[?OutputKey=='RoleArn'].OutputValue" --output text)"
+role_arn="$(aws cloudformation describe-stacks --stack-name "${stack_name}" --region us-east-1 --query "Stacks[0].Outputs[?OutputKey=='RoleArn'].OutputValue" --output text)"
 buildkite-agent meta-data set service-role-arn "${role_arn}"
