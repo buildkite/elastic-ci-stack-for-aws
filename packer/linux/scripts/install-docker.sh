ddda95302319951026d9a56bae49555f9e8e7e4a
Orien Madgwick <_@orien.io>
2022-03-31 09:00:01 +1100

    Bump Docker Buildx from 0.7.1 to 0.8.2
    0.8.0 release notes: https://github.com/docker/buildx/releases/tag/v0.8.0
    0.8.1 release notes: https://github.com/docker/buildx/releases/tag/v0.8.1
    0.8.2 release notes: https://github.com/docker/buildx/releases/tag/v0.8.2

--- packer/linux/scripts/install-docker.sh
+++ packer/linux/scripts/install-docker.sh
@@ -4,7 +4,7 @@ set -eu -o pipefail
 DOCKER_VERSION=20.10.14
 DOCKER_RELEASE="stable"
 DOCKER_COMPOSE_VERSION=1.29.2
-DOCKER_BUILDX_VERSION="0.7.1"
+DOCKER_BUILDX_VERSION="0.8.2"
 MACHINE=$(uname -m)
 
 # This performs a manual install of Docker.
