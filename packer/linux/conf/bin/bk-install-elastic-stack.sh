ac09fc37d6b0fa9fea8511ef0dac6b63baff2db0
Narthana Epa <narthana.epa@gmail.com>
2022-10-31 17:23:29 +1100

    Add a lifecycle hook on buildkite agent start

--- packer/linux/conf/bin/bk-install-elastic-stack.sh
+++ packer/linux/conf/bin/bk-install-elastic-stack.sh
@@ -183,6 +183,7 @@ no-color=true
 disconnect-after-idle-timeout=${BUILDKITE_SCALE_IN_IDLE_PERIOD}
 disconnect-after-job=${BUILDKITE_TERMINATE_INSTANCE_AFTER_JOB}
 tracing-backend=${BUILDKITE_AGENT_TRACING_BACKEND}
+aws-asg-lifecycle-hook-name=buildkite-agent-start
 EOF
 
 if [[ "${BUILDKITE_ENV_FILE_URL}" != "" ]]; then
