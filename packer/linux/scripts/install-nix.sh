3a7a6356da4bfbd0ecee99d6240d4ee5b4f5fbcc
Christian Sakshaug <christian@dx.no>
2022-11-11 09:16:42 +0000

    clean up commits

--- packer/linux/scripts/install-nix.sh
+++ packer/linux/scripts/install-nix.sh
@@ -2,3 +2,4 @@
 set -eu -o pipefail
 echo "Installing nix"
 sh <(curl -L https://nixos.org/nix/install) --daemon
+sudo echo ". /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh" >> /var/lib/buildkite-agent/.bash_profile
