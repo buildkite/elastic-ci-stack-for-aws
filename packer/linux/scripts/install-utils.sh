458bfb5b9c040a190c7909c28a2f7243943ae152
Narthana Epa <narthana.epa@gmail.com>
2023-01-18 15:17:08 +1100

    Install git from source

--- packer/linux/scripts/install-utils.sh
+++ packer/linux/scripts/install-utils.sh
@@ -22,5 +22,21 @@ sudo mv /tmp/conf/bin/bk-* /usr/local/bin
 echo "Configuring awscli to use v4 signatures..."
 sudo aws configure set s3.signature_version s3v4
 
+# https://git-scm.com/book/en/v2/Getting-Started-Installing-Git#_installing_from_source
+GIT_VERSION=2.39.1
+echo "Installing git $GIT_VERSION from source"
+sudo yum -y groupinstall "Development Tools"
+pushd "$(mktemp -d)"
+curl -sSL "https://mirrors.edge.kernel.org/pub/software/scm/git/git-${GIT_VERSION}.tar.xz" | tar xJ
+pushd git-"$GIT_VERSION"
+make configure
+./configure --prefix=/usr
+make all
+sudo make install
+git --version
+sudo yum -y groupremove "Development Tools"
+popd
+popd
+
 echo "Installing goss for system validation..."
 curl -fsSL https://goss.rocks/install | GOSS_VER=v0.3.20 sudo sh
