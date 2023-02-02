2305697ab117339f310d7d89c76b529b67f41898
Narthana Epa <narthana.epa@gmail.com>
2023-02-02 16:14:35 +1100

    Build git 2.39.1 with rpm

--- packer/linux/scripts/install-utils.sh
+++ packer/linux/scripts/install-utils.sh
@@ -22,20 +22,32 @@ sudo mv /tmp/conf/bin/bk-* /usr/local/bin
 echo "Configuring awscli to use v4 signatures..."
 sudo aws configure set s3.signature_version s3v4
 
-# https://git-scm.com/book/en/v2/Getting-Started-Installing-Git#_installing_from_source
 GIT_VERSION=2.39.1
-echo "Installing git $GIT_VERSION from source"
-sudo yum -y groupinstall "Development Tools"
+echo "Installing git $GIT_VERSION"
+sudo yum install -y rpm-build yum-utils
+sudo amazon-linux-extras install epel -y
+
 pushd "$(mktemp -d)"
-curl -sSL "https://mirrors.edge.kernel.org/pub/software/scm/git/git-${GIT_VERSION}.tar.xz" | tar xJ
-pushd git-"$GIT_VERSION"
-make configure
-./configure --prefix=/usr
-make all
-sudo make install
-git --version
-sudo yum -y groupremove "Development Tools"
+yumdownloader --source git-2.38.1
+rpm -ivh git-2.38.1-1.amzn2.0.1.src.rpm
 popd
+
+pushd "$HOME/rpmbuild/SOURCES"
+curl -sSLOJ "https://mirrors.edge.kernel.org/pub/software/scm/git/git-${GIT_VERSION}.tar.xz"
+curl -sSLOJ "https://mirrors.edge.kernel.org/pub/software/scm/git/git-${GIT_VERSION}.tar.sign"
+popd
+
+pushd "$HOME/rpmbuild/SPECS"
+# we don't want to build with docs, it pulls in a lot of dependencies we do not want
+sed -i '/^%if %{with docs}$/,/^# endif with docs$/d' git.spec
+# bump the version
+sed -i 's/tarball_version 2.38.1/tarball_version '"${GIT_VERSION}"'/;s/Version:        2.38.1/Version:        '"${GIT_VERSION}/" git.spec
+sudo yum-builddep -y git.spec
+rpmbuild -ba --nocheck --nodeps --without docs --without tests git.spec
+popd
+
+pushd "$HOME/rpmbuild/RPMS"
+sudo yum localinstall -y noarch/* "$(uname -m)"/*
 popd
 
 echo "Installing goss for system validation..."
