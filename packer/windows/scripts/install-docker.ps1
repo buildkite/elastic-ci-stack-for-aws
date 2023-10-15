d408419fc409f802f19531bc3638c60fd77de963
Narthana Epa <narthana.epa@gmail.com>
2023-10-15 16:46:34 +1100

    Don't check docker version
    Docker will be tested in the tests

--- packer/windows/scripts/install-docker.ps1
+++ packer/windows/scripts/install-docker.ps1
@@ -3,10 +3,9 @@ $ErrorActionPreference = "Stop"
 
 $docker_compose_version="1.29.2"
 
-Write-Output "Install docker"
+Write-Output "Installing docker..."
 Invoke-WebRequest -UseBasicParsing "https://raw.githubusercontent.com/microsoft/Windows-Containers/Main/helpful_tools/Install-DockerCE/install-docker-ce.ps1" -o install-docker-ce.ps1
 .\install-docker-ce.ps1
-docker --version
 
 Write-Output "Installing docker-compose"
 choco install -y docker-compose --version $docker_compose_version
