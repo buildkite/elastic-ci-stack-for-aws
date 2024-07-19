1d6efec861a9d490c7b106200ab762e150051f87
Stephen Bell <stephen@buildkite.com>
2024-07-19 14:26:11 +1200

    Upgrade test splitter client to v0.7.3

--- packer/linux/scripts/install-buildkite-test-splitter.sh
+++ packer/linux/scripts/install-buildkite-test-splitter.sh
@@ -7,9 +7,9 @@ aarch64) ARCH=arm64 ;;
 *) ARCH=unknown ;;
 esac
 
-TEST_SPLITTER_VERSION=0.7.2
+TEST_SPLITTER_VERSION=0.7.3
 echo "Downloading test-splitter v${TEST_SPLITTER_VERSION}..."
 sudo curl -Lsf -o /usr/bin/test-splitter \
-  "https://github.com/buildkite/test-splitter/releases/download/v0.7.2/test-splitter-linux-${ARCH}"
+  "https://github.com/buildkite/test-splitter/releases/download/v${TEST_SPLITTER_VERSION}/test-splitter-linux-${ARCH}"
 sudo chmod +x /usr/bin/test-splitter
 test-splitter --version
