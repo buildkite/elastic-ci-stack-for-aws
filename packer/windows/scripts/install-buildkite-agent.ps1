26ee6016abb91b370186fcdca723f11fe98a6c44
PriyaSudip <40382124+PriyaSudip@users.noreply.github.com>
2025-03-12 17:13:37 +1100

    bump agent version to v3.94.0
    Bump agent version to v3.94.0

--- packer/windows/scripts/install-buildkite-agent.ps1
+++ packer/windows/scripts/install-buildkite-agent.ps1
@@ -1,7 +1,7 @@
 # Stop script execution when a non-terminating error occurs
 $ErrorActionPreference = "Stop"
 
-$AGENT_VERSION = "3.93.1"
+$AGENT_VERSION = "3.94.0"
 
 Write-Output "Creating bin dir..."
 New-Item -ItemType directory -Path C:\buildkite-agent\bin
