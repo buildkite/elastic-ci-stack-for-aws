a3bb8f899d4cf55b968edbc2cff90dbf27e500bd
bkarthik <bkarthik@applied.co>
2025-08-20 18:39:50 -0700

    this ami can run can

--- packer/linux/scripts/install-socketcan.sh
+++ packer/linux/scripts/install-socketcan.sh
@@ -3,3 +3,13 @@ set -eux
 
 # 1. install can-utils
 sudo dnf install -yq can-utils
+
+sudo dnf install -y "kernel-modules-extra-$(uname -r)" \
+                      "kernel-headers-$(uname -r)" \
+                      "kernel-devel-$(uname -r)"
+
+sudo dnf install -y "kernel-modules-extra-$(uname -r)" \
+                      "kernel-headers-$(uname -r)" \
+                      "kernel-devel-$(uname -r)"
+
+sudo modprobe vcan
