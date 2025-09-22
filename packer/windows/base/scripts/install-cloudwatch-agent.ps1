de929ab6bdd05003339e68409ab2b277c58222eb
Łukasz Jendrysik <lukasz@buildkite.com>
2025-09-22 15:48:59 +0200

    Fix Windows base AMI build

--- packer/windows/base/scripts/install-cloudwatch-agent.ps1
+++ packer/windows/base/scripts/install-cloudwatch-agent.ps1
@@ -5,10 +5,11 @@ Write-Output "Downloading amazon cloudwatch agent..."
 Invoke-WebRequest -OutFile C:\packer-temp\amazon-cloudwatch-agent.msi -Uri "https://s3.amazonaws.com/amazoncloudwatch-agent/windows/amd64/latest/amazon-cloudwatch-agent.msi"
 
 Write-Output "Installing amazon cloudwatch agent..."
-Start-Process C:\packer-temp\amazon-cloudwatch-agent.msi -Wait
+$process = Start-Process msiexec -ArgumentList "/i", "C:\packer-temp\amazon-cloudwatch-agent.msi", "/quiet", "/norestart" -Wait -PassThru
+If ($process.ExitCode -ne 0) { Exit $process.ExitCode }
 
-Write-Output "Setting amazon cloudwatch agent start type to delayed-auto..."
-sc.exe config AmazonCloudWatchAgent start= delayed-auto
+Write-Output "Setting amazon cloudwatch agent start type to manual (will be configured in stack layer)..."
+sc.exe config AmazonCloudWatchAgent start= demand
 If ($lastexitcode -ne 0) { Exit $lastexitcode }
 
 Write-Output "CloudWatch agent installed. Configuration will be done in stack layer."
