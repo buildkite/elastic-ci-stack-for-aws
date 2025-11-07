add31b1467f9e34ba5af32d1e22ccdc317bd4c62
Joe Coleman <joe@buildkite.com>
2025-11-07 16:15:12 +0000

    Update install-utils.sh

--- packer/linux/base/scripts/install-utils.sh
+++ packer/linux/base/scripts/install-utils.sh
@@ -73,6 +73,8 @@ curl -sSL https://github.com/git-lfs/git-lfs/releases/download/v${GIT_LFS_VERSIO
 sudo git-lfs-${GIT_LFS_VERSION}/install.sh
 popd
 
+#TEST
+
 # See https://github.com/goss-org/goss/releases for release versions
 GOSS_VERSION=v0.3.23
 echo "Installing goss $GOSS_VERSION for system validation..."
