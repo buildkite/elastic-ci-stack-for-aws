66bbd529f12936cf607f5367f7faad7a914c1ad0
Joe Coleman <joe@buildkite.com>
2025-11-26 16:20:18 +0000

    Handle missing Docker config in disk space check
    Updating disk space check to gracefully handle cases where /etc/docker/daemon.json is missing or unreadable by defaulting DOCKER_DIR to /var/lib/docker.

--- packer/linux/stack/conf/bin/bk-check-disk-space.sh
+++ packer/linux/stack/conf/bin/bk-check-disk-space.sh
@@ -4,7 +4,10 @@ set -euo pipefail
 DISK_MIN_AVAILABLE=${DISK_MIN_AVAILABLE:-5242880} # 5GB
 DISK_MIN_INODES=${DISK_MIN_INODES:-250000}        # docker needs lots
 
-DOCKER_DIR="$(jq -r '."data-root" // "/var/lib/docker"' /etc/docker/daemon.json)"
+DOCKER_DIR="$(jq -r '."data-root" // "/var/lib/docker"' /etc/docker/daemon.json || true)"
+if [[ -z "$DOCKER_DIR" ]]; then
+  DOCKER_DIR="/var/lib/docker"
+fi
 
 disk_avail=$(df -k --output=avail "$DOCKER_DIR" | tail -n1)
 
