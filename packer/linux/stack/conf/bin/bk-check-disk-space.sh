ca9ccaa972e03876745f3f8b14a6cd24e6cdc047
Pete Tomasik <senate6268@gmail.com>
2026-01-29 11:25:28 -0500

    Fix disk cleanup for multi-agent instances

--- packer/linux/stack/conf/bin/bk-check-disk-space.sh
+++ packer/linux/stack/conf/bin/bk-check-disk-space.sh
@@ -21,15 +21,18 @@ if [[ $disk_avail -lt $DISK_MIN_AVAILABLE ]]; then
 
   # Last resort for clearing space with build directory cleanup (if enabled)
   if [[ "${BUILDKITE_PURGE_BUILDS_ON_DISK_FULL:-false}" == "true" ]]; then
-    echo "Purging all build directories to reclaim disk space..."
-    rm -rf "${BUILDKITE_AGENT_BUILD_PATH:-/var/lib/buildkite-agent/builds}"/*
+    # Extract the agent-specific directory to avoid disrupting other agents on the same instance
+    AGENT_ORG_PIPELINE_DIR="${BUILDKITE_BUILD_CHECKOUT_PATH#"${BUILDKITE_BUILD_PATH}/"}"
+    AGENT_DIR="${AGENT_ORG_PIPELINE_DIR%%/*}"
+    AGENT_BUILD_DIR="${BUILDKITE_BUILD_PATH}/${AGENT_DIR}"
+
+    echo "Purging builds in ${AGENT_BUILD_DIR}"
+    rm -rf "${AGENT_BUILD_DIR:?}"/*
+
     disk_avail=$(df -k --output=avail "$DOCKER_DIR" | tail -n1)
     disk_avail_human=$(numfmt --to=iec-i --suffix=B --from-unit=1024 "${disk_avail}")
     echo "Disk space free after build purge: ${disk_avail_human} (${disk_avail}KB)"
-    if [[ $disk_avail -ge $DISK_MIN_AVAILABLE ]]; then
-      echo "Disk space sufficient after build purge. Continuing."
-      exit 0
-    else
+    if [[ $disk_avail -lt $DISK_MIN_AVAILABLE ]]; then
       echo "Insufficient disk space remaining after build purge." >&2
       exit 1
     fi
